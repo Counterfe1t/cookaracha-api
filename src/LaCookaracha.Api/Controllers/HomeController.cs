4c42633d27e17549cd26a8d7692886ce3ee19177
Counterfe1t <karoljanbarcz@gmail.com>
2024-12-04 23:49:21 +0100

    Add solution & webapi project, add .gitignore file

---
+++ src/LaCookaracha.Api/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LaCookaracha.Api.Controllers;
+
+[ApiController]
+[Route("api/home")]
+public class HomeController
+{
+    [HttpGet]
+    public ActionResult<string> Get() => "LaCookaracha.Api";
+}
