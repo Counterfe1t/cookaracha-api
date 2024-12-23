c5e64b0267314d81b52e36bb38fcbcc797a1adfa
Counterfe1t <karoljanbarcz@gmail.com>
2024-12-24 00:34:46 +0100

    Add basic unit tests to product entity

---
+++ tests/Cookaracha.UnitTests/Entities/ProductTests.cs
@@ -0,0 +1,38 @@
+﻿using Cookaracha.Core.Entities;
+using Cookaracha.Core.Exceptions;
+using Shouldly;
+
+namespace Cookaracha.UnitTests.Entities;
+
+public class ProductTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("product_name_longer_than_fifty_characters_should_throw_exception")]
+    public void ChangeProductName_ProductNameIsInvalid_ShouldThrowException(string invalidName)
+    {
+        // arrange
+        var product = new Product(Guid.NewGuid(), "dummy");
+
+        // act
+        var exception = Record.Exception(() => product.ChangeProductName(invalidName));
+
+        // assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<InvalidProductNameException>();
+    }
+
+    [Fact]
+    public void ChangeProductName_ProductNameIsValid_ShouldChangeProductName()
+    {
+        // arrange
+        var product = new Product(Guid.NewGuid(), "foo");
+        var newProductName = "bar";
+
+        // act
+        product.ChangeProductName(newProductName);
+
+        // assert
+        product.Name.Value.ShouldBe(newProductName);
+    }
+}
