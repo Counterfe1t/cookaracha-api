ba2739a2518c89e04314783a563f16a021df9c80
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-12 21:47:29 +0100

    Add simple grocery list entity

---
+++ src/Cookaracha.Core/Entities/GroceryList.cs
@@ -0,0 +1,13 @@
+﻿using Cookaracha.Core.ValueObjects;
+
+namespace Cookaracha.Core.Entities;
+
+public sealed class GroceryList
+{
+    public GroceryListId Id { get; private set; }
+
+    public GroceryList(GroceryListId id)
+    {
+        Id = id;
+    }
+}
--- src/Cookaracha.Core/ValueObjects/GroceryListId.cs
+++ src/Cookaracha.Core/ValueObjects/GroceryListId.cs
@@ -2,11 +2,11 @@
 
 namespace Cookaracha.Core.ValueObjects;
 
-public sealed record ProductId
+public sealed record GroceryListId
 {
     public Guid Value { get; }
 
-    public ProductId(Guid value)
+    public GroceryListId(Guid value)
     {
         if (value == Guid.Empty)
             throw new InvalidEntityIdException(value);
@@ -14,9 +14,9 @@ public sealed record ProductId
         Value = value;
     }
 
-    public static implicit operator Guid(ProductId id)
+    public static implicit operator Guid(GroceryListId id)
         => id.Value;
 
-    public static implicit operator ProductId(Guid value)
+    public static implicit operator GroceryListId(Guid value)
         => new(value);
 }
