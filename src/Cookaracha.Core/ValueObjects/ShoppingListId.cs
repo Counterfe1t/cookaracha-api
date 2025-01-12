a854aee517fb09b28b34f5dbf01733fb63210a28
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-12 21:47:29 +0100

    Add simple shopping list entity

--- src/Cookaracha.Core/ValueObjects/ShoppingListId.cs
+++ src/Cookaracha.Core/ValueObjects/ShoppingListId.cs
@@ -2,11 +2,11 @@
 
 namespace Cookaracha.Core.ValueObjects;
 
-public sealed record ProductId
+public sealed record ShoppingListId
 {
     public Guid Value { get; }
 
-    public ProductId(Guid value)
+    public ShoppingListId(Guid value)
     {
         if (value == Guid.Empty)
             throw new InvalidEntityIdException(value);
@@ -14,9 +14,9 @@ public sealed record ProductId
         Value = value;
     }
 
-    public static implicit operator Guid(ProductId id)
+    public static implicit operator Guid(ShoppingListId id)
         => id.Value;
 
-    public static implicit operator ProductId(Guid value)
+    public static implicit operator ShoppingListId(Guid value)
         => new(value);
 }
