2553065c07e13611ee830fc1401f4693353da939
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-17 00:06:09 +0100

    Add grocery lists controller with a simple get request

---
+++ src/Cookaracha.Api/Controllers/GroceryListsController.cs
@@ -0,0 +1,22 @@
+﻿using Cookaracha.Application.Abstractions;
+using Cookaracha.Application.DTO;
+using Cookaracha.Application.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cookaracha.Api.Controllers;
+
+[ApiController]
+[Route("api/grocery-lists")]
+public class GroceryListsController : ControllerBase
+{
+    private readonly IQueryHandler<GetGroceryList, GroceryListDto> _getGroceryListHandler;
+
+    public GroceryListsController(IQueryHandler<GetGroceryList, GroceryListDto> getGroceryListHandler)
+    {
+        _getGroceryListHandler = getGroceryListHandler;
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<GroceryListDto>> Get([FromRoute] Guid id)
+        => Ok(await _getGroceryListHandler.HandleAsync(new GetGroceryList(id)));
+}
