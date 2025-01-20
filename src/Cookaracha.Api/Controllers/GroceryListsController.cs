6b89461a361911a8fc5e668670b33245d2234c78
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-20 03:26:00 +0100

    Add endpoint to fetch all grocery lists

--- src/Cookaracha.Api/Controllers/GroceryListsController.cs
+++ src/Cookaracha.Api/Controllers/GroceryListsController.cs
@@ -9,12 +9,20 @@ namespace Cookaracha.Api.Controllers;
 [Route("api/grocery-lists")]
 public class GroceryListsController : ControllerBase
 {
+    private readonly IQueryHandler<GetGroceryLists, IEnumerable<GroceryListDto>> _getGroceryListsHandler;
     private readonly IQueryHandler<GetGroceryList, GroceryListDto> _getGroceryListHandler;
 
-    public GroceryListsController(IQueryHandler<GetGroceryList, GroceryListDto> getGroceryListHandler)
+    public GroceryListsController(
+        IQueryHandler<GetGroceryLists, IEnumerable<GroceryListDto>> getGroceryListsHandler,
+        IQueryHandler<GetGroceryList, GroceryListDto> getGroceryListHandler)
     {
+        _getGroceryListsHandler = getGroceryListsHandler;
         _getGroceryListHandler = getGroceryListHandler;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<GroceryListDto>>> Get([FromQuery] GetGroceryLists query)
+        => Ok(await _getGroceryListsHandler.HandleAsync(query));
 
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<GroceryListDto>> Get([FromRoute] Guid id)
