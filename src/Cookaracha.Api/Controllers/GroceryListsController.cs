b1905fb7e46b41bb1484dfb919a4de31ed05f14d
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-24 00:00:27 +0100

    Add endpoint to create new grocery list

--- src/Cookaracha.Api/Controllers/GroceryListsController.cs
+++ src/Cookaracha.Api/Controllers/GroceryListsController.cs
@@ -1,4 +1,5 @@
 ﻿using Cookaracha.Application.Abstractions;
+using Cookaracha.Application.Commands;
 using Cookaracha.Application.DTO;
 using Cookaracha.Application.Queries;
 using Microsoft.AspNetCore.Mvc;
@@ -12,12 +13,16 @@ public class GroceryListsController : ControllerBase
     private readonly IQueryHandler<GetGroceryLists, IEnumerable<GroceryListDto>> _getGroceryListsHandler;
     private readonly IQueryHandler<GetGroceryList, GroceryListDto> _getGroceryListHandler;
 
+    private readonly ICommandHandler<CreateGroceryList> _createGroceryListHandler;
+
     public GroceryListsController(
         IQueryHandler<GetGroceryLists, IEnumerable<GroceryListDto>> getGroceryListsHandler,
-        IQueryHandler<GetGroceryList, GroceryListDto> getGroceryListHandler)
+        IQueryHandler<GetGroceryList, GroceryListDto> getGroceryListHandler,
+        ICommandHandler<CreateGroceryList> createGroceryListHandler)
     {
         _getGroceryListsHandler = getGroceryListsHandler;
         _getGroceryListHandler = getGroceryListHandler;
+        _createGroceryListHandler = createGroceryListHandler;
     }
 
     [HttpGet]
@@ -27,4 +32,13 @@ public class GroceryListsController : ControllerBase
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<GroceryListDto>> Get([FromRoute] Guid id)
         => Ok(await _getGroceryListHandler.HandleAsync(new GetGroceryList(id)));
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] CreateGroceryList command)
+    {
+        command = command with { Id = Guid.NewGuid() };
+        await _createGroceryListHandler.HandleAsync(command);
+
+        return CreatedAtAction(nameof(Get), new { command.Id }, null);
+    }
 }
