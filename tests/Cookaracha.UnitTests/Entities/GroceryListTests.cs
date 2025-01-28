437d592a5cd32d12c83da4bf8a47abffe3022afc
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-29 00:15:09 +0100

    Add unit tests to grocery list entity

---
+++ tests/Cookaracha.UnitTests/Entities/GroceryListTests.cs
@@ -0,0 +1,43 @@
+﻿using Cookaracha.Core.Entities;
+using Cookaracha.Core.Exceptions;
+using Shouldly;
+
+namespace Cookaracha.UnitTests.Entities;
+
+public class GroceryListTests
+{
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData("f")]
+    [InlineData("grocery_list_name_longer_than_fifty_characters_should_throw_exception")]
+    public void ChangeGroceryListName_GroceryListNameIsInvalid_ShouldThrowException(string invalidName)
+    {
+        // arrange
+        var groceryList = new GroceryList(Guid.NewGuid(), "dummy");
+
+        // act
+        var exception = Record.Exception(() => groceryList.ChangeGroceryListName(invalidName));
+
+        // assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<InvalidGroceryListNameException>();
+    }
+
+    [Theory]
+    [InlineData("DUMMY", "dummy")]
+    [InlineData("Dummy", "dummy")]
+    [InlineData(" dummy ", "dummy")]
+    [InlineData(" Dummy  Grocery List ", "dummy grocery list")]
+    public void ChangeGroceryListName_GroceryListNameIsValid_ShouldSanitizeAndChangeGroceryListName(string newValue, string expectedValue)
+    {
+        // arrange
+        var groceryList = new GroceryList(Guid.NewGuid(), "dummy");
+
+        // act
+        groceryList.ChangeGroceryListName(newValue);
+
+        // assert
+        groceryList.Name.Value.ShouldBe(expectedValue);
+    }
+}
