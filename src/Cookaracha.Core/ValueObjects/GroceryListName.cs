05ec732915513c0a8f04ad30ae7c97c67147e767
Counterfe1t <karoljanbarcz@gmail.com>
2025-01-31 00:45:32 +0100

    Overload grocery list name equality/inequality operator for smooth string comparison

--- src/Cookaracha.Core/ValueObjects/GroceryListName.cs
+++ src/Cookaracha.Core/ValueObjects/GroceryListName.cs
@@ -12,13 +12,9 @@ public sealed record GroceryListName
 
     public GroceryListName(string value)
     {
-        if (string.IsNullOrWhiteSpace(value))
-            throw new InvalidGroceryListNameException(value);
-
-        if (value.Length < MinLength || value.Length > MaxLength)
-            throw new InvalidGroceryListNameException(value);
-
-        Value = Sanitize(value);
+        Value = IsValid(value)
+            ? Sanitize(value)
+            : throw new InvalidGroceryListNameException(value);
     }
 
     public static implicit operator string(GroceryListName name)
@@ -27,7 +23,31 @@ public sealed record GroceryListName
     public static implicit operator GroceryListName(string value)
         => new(value);
 
+    public static bool operator ==(GroceryListName name, string value)
+        => name.Value == value;
+
+    public static bool operator !=(GroceryListName name, string value)
+        => name.Value != value;
+
+    public static bool operator ==(string value, GroceryListName name)
+        => name.Value == value;
+
+    public static bool operator !=(string value, GroceryListName name)
+        => name.Value != value;
+
+    // TODO: Improve and move validation to a core utility class
+    private static bool IsValid(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            return false;
+
+        if (value.Length < MinLength || value.Length > MaxLength)
+            return false;
+
+        return true;
+    }
+
     // TODO: Rename and move this method to a core utility class
     private static string Sanitize(string value)
-        => Regex.Replace(value.ToLower().Trim(), @"\s+", " ");
+        => Regex.Replace(value.Trim(), @"\s+", " ");
 }
