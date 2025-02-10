96feb68a136b4678bb2003edfbd5188d19ce7d31
Counterfe1t <karoljanbarcz@gmail.com>
2025-02-10 23:57:53 +0100

    Add summary comments to grocery list repository interface

--- src/Cookaracha.Core/Repositories/IGroceryListsRepository.cs
+++ src/Cookaracha.Core/Repositories/IGroceryListsRepository.cs
@@ -4,9 +4,34 @@ namespace Cookaracha.Core.Repositories;
 
 public interface IGroceryListsRepository
 {
+    /// <summary>
+    /// Get all grocery lists.
+    /// </summary>
+    /// <returns>Collection of <see cref="GroceryList"/> objects.</returns>
     Task<IEnumerable<GroceryList>> GetAllAsync();
+
+    /// <summary>
+    /// Get grocery list by ID.
+    /// </summary>
+    /// <param name="id">Grocery list ID.</param>
+    /// <returns><see cref="GroceryList"/> object.</returns>
     Task<GroceryList?> GetAsync(Guid id);
+
+    /// <summary>
+    /// Add grocery list.
+    /// </summary>
+    /// <param name="groceryList"><see cref="GroceryList"/> object.</param>
     Task AddAsync(GroceryList groceryList);
+
+    /// <summary>
+    /// Update grocery list.
+    /// </summary>
+    /// <param name="groceryList"><see cref="GroceryList"/> object.</param>
     Task UpdateAsync(GroceryList groceryList);
+
+    /// <summary>
+    /// Delete grocery list.
+    /// </summary>
+    /// <param name="groceryList"><see cref="GroceryList"/> object.</param>
     Task DeleteAsync(GroceryList groceryList);
 }
