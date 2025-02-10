93e9ba9b0fd07c66bd6dbf17a0dee70f64ea7e8d
Counterfe1t <karoljanbarcz@gmail.com>
2025-02-11 00:03:20 +0100

    Add summary comments to products repository interface

--- src/Cookaracha.Core/Repositories/IProductsRepository.cs
+++ src/Cookaracha.Core/Repositories/IProductsRepository.cs
@@ -4,9 +4,34 @@ namespace Cookaracha.Core.Repositories;
 
 public interface IProductsRepository
 {
+    /// <summary>
+    /// Get all products.
+    /// </summary>
+    /// <returns>Collection of <see cref="Product"/> objects.</returns>
     Task<IEnumerable<Product>> GetAllAsync();
+
+    /// <summary>
+    /// Get product by ID.
+    /// </summary>
+    /// <param name="id">Product ID.</param>
+    /// <returns><see cref="Product"/> object.</returns>
     Task<Product?> GetAsync(Guid id);
+
+    /// <summary>
+    /// Add product.
+    /// </summary>
+    /// <param name="product"><see cref="Product"/> object.</param>
     Task AddAsync(Product product);
+
+    /// <summary>
+    /// Update product.
+    /// </summary>
+    /// <param name="product"><see cref="Product"/> object.</param>
     Task UpdateAsync(Product product);
+
+    /// <summary>
+    /// Delete product.
+    /// </summary>
+    /// <param name="product"><see cref="Product"/> object.</param>
     Task DeleteAsync(Product product);
 }
