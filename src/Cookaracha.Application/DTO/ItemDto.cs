f1c698df227e5f61b0c8b47ac0ef3778d6a68a11
Counterfe1t <karoljanbarcz@gmail.com>
2025-07-13 00:25:38 +0200

    Add item data transfer object

---
+++ src/Cookaracha.Application/DTO/ItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Cookaracha.Application.DTO;
+
+public class ItemDto
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public double Quantity { get; set; }
+}
