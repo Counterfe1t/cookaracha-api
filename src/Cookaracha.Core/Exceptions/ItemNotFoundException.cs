fe85c771b81eab110bcf18e19fb5f2bee3e112d1
Counterfe1t <karoljanbarcz@gmail.com>
2025-07-31 00:55:26 +0200

    Add item not found custom exception

---
+++ src/Cookaracha.Core/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Cookaracha.Core.Exceptions;
+
+public sealed class ItemNotFoundException : CustomException
+{
+    public Guid ItemId { get; }
+
+    public ItemNotFoundException(Guid itemId)
+        : base($"Item with ID: '{itemId}' was not found.")
+    {
+        StatusCode = (int)HttpStatusCode.NotFound;
+        ItemId = itemId;
+    }
+}
