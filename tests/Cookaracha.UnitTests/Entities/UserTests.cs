9cbbfaf128a201cd67dae3e23f804d9095135d42
Counterfe1t <karoljanbarcz@gmail.com>
2025-09-16 16:31:11 +0200

    Add unit tests to change user password method

--- tests/Cookaracha.UnitTests/Entities/UserTests.cs
+++ tests/Cookaracha.UnitTests/Entities/UserTests.cs
@@ -78,7 +78,7 @@ public class UserTests
     [Theory]
     [InlineData("valid@email.address", "valid@email.address")]
     [InlineData("VALID@EMAIL.ADDRESS", "valid@email.address")]
-    public void ChangeEmail_EmailIsValid_ShouldSanitizeAndChangeUserName(string newValue, string expectedValue)
+    public void ChangeEmail_EmailIsValid_ShouldSanitizeAndChangeUserEmail(string newValue, string expectedValue)
     {
         // arrange
         var User = new User(
@@ -93,5 +93,47 @@ public class UserTests
 
         // assert
         User.Email.Value.ShouldBe(expectedValue);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("   ")]
+    [InlineData("short")]
+    [InlineData("password_name_longer_than_two_hundred_characters_should_throw_exception_8c4bca6068d442dc928f7344f13ada60ce0a8858203b47c18a943bd6cdb44f7b2fb7afa6ced14e7c8f588f3d251e8f3a7aa1e39074084827aa1e3907408482abb")]
+    public void ChangePassword_PasswordIsInvalid_ShouldThrowException(string invalidPassword)
+    {
+        // arrange
+        var User = new User(
+            Guid.NewGuid(),
+            DateTimeOffset.UtcNow,
+            "dummy user name",
+            "dummy@cookaracha.net",
+            "dummy password");
+
+        // act
+        var exception = Record.Exception(() => User.ChangePassword(invalidPassword));
+
+        // assert
+        exception.ShouldNotBeNull();
+        exception.ShouldBeOfType<InvalidPasswordException>();
+    }
+
+    [Fact]
+    public void ChangePassword_PasswordIsValid_ShouldSanitizeAndChangeUserPassword()
+    {
+        // arrange
+        var expectedValue = "valid_password";
+        var User = new User(
+            Guid.NewGuid(),
+            DateTimeOffset.UtcNow,
+            "dummy username",
+            "dummy@cookaracha.net",
+            "dummy password");
+
+        // act
+        User.ChangePassword(expectedValue);
+
+        // assert
+        User.Password.Value.ShouldBe(expectedValue);
     }
 }
